203917710b93e471dfe6e681de8221c4cc03b561
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-10-25 07:55:16 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -58,4 +58,5 @@ def process_image(image_data):
    buf = io.BytesIO()
    plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
    buf.seek(0)
+   img_base64 = base64.b64encode(buf.getvalue()).decode('utf-8')
 
